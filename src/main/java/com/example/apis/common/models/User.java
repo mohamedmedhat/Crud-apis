3b851efde07f56d8b3a0cf6adae7b4097afb84c5
mohamedmedhat <mmr973320@gmail.com>
2025-04-04 14:08:24 +0200

    (add): make the common model and repository

---
+++ src/main/java/com/example/apis/common/models/User.java
@@ -0,0 +1,26 @@
+package com.example.apis.common.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String name;
+    private int age;
+    private BigDecimal rate;
+}
---
+++ src/main/java/com/example/apis/common/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.apis.common.repositories;
+
+import com.example.apis.common.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
